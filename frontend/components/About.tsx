700e35392b228edb0dd655d266b897fb206cad80
dtulumidis <dextavision@gmail.com>
2022-06-05 11:01:48 +0200

    changes font size

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
                     />
                 </div>
                 <div className='lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center'>
-                    <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
+                    <h1 className='title-font sm:text-5xl text-3xl mb-4 font-medium text-white'>
                         How Decetable works?
                     </h1>
                     <p className='mb-8 leading-relaxed'>
